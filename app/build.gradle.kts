d0e1a49c26db333f8a7b41918cae117ce1a27c91
SpectrumAnalyzer <profitsw2000@gmail.com>
2026-03-27 16:39:41 +0300

    В gradle модуля app добавлена ссылка на модуль core.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":core"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,7 +7,6 @@ android {
 }
 
 dependencies {
-    implementation(project(":core"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
