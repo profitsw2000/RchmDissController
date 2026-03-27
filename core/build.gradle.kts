c524784d35ceba576be801db22a421ae61171913
SpectrumAnalyzer <profitsw2000@gmail.com>
2026-03-27 13:37:03 +0300

    Основные папки с ресурсами перенесены в модуль core из модуля app.

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,9 +7,13 @@ android {
 }
 
 dependencies {
+    implementation(project(":core"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
