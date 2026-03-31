69901c1aa0a981df6b18d03cf596986bcbbc5031
SpectrumAnalyzer <profitsw2000@gmail.com>
2026-03-31 16:49:31 +0300

    Добавлены цвета для темы приложения.

--- navigator/build.gradle.kts
+++ navigator/build.gradle.kts
@@ -1,33 +1,9 @@
 plugins {
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.rdc.main.lib.gradle.plugin)
 }
 
 android {
     namespace = "ru.profitsw2000.navigator"
-    compileSdk {
-        version = release(36)
-    }
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
 }
 
 dependencies {
