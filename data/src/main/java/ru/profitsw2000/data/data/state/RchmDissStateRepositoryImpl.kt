1e5733b954726340e46c31e4f5c452848146f4fa
SpectrumAnalyzer <profitsw2000@gmail.com>
2026-05-06 16:20:46 +0300

    Создан класс для имплементации интерфейса RchmDissStateRepository.

---
+++ data/src/main/java/ru/profitsw2000/data/data/state/RchmDissStateRepositoryImpl.kt
@@ -0,0 +1,41 @@
+package ru.profitsw2000.data.data.state
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import ru.profitsw2000.data.domain.state.RchmDissStateRepository
+import ru.profitsw2000.data.model.bluetooth.state.rcd.RchmDissState
+import ru.profitsw2000.data.model.rcd.RcdInputPacketType
+
+class RchmDissStateRepositoryImpl() : RchmDissStateRepository {
+    private val repositoryScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+    private val _rchmDissState: MutableStateFlow<RchmDissState> = MutableStateFlow(RchmDissState())
+    override val rchmDissState: StateFlow<RchmDissState>
+        get() = _rchmDissState
+    private val _lastPacket: MutableSharedFlow<RcdInputPacketType> = MutableStateFlow(RcdInputPacketType.InvalidInputPacket)
+    override val lastPacket: SharedFlow<RcdInputPacketType>
+        get() = _lastPacket
+
+    override fun updateTransmitterModuleState(byteArray: ByteArray) {
+        repositoryScope.launch {
+            _lastPacket.emit(RcdInputPacketType.TransmitterStateInputPacket)
+        }
+    }
+
+    override fun updateReceiverModuleState(byteArray: ByteArray) {
+        repositoryScope.launch {
+            _lastPacket.emit(RcdInputPacketType.ReceiverStateInputPacket)
+        }
+    }
+
+    override fun updateSynthesizerModuleState(byteArray: ByteArray) {
+        repositoryScope.launch {
+            _lastPacket.emit(RcdInputPacketType.SynthesizerStateInputPacket)
+        }
+    }
+}
