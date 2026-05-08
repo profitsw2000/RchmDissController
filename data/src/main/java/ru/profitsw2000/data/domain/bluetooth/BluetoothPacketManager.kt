fbb870b5ac9f70ebd7c431d7f33dca544f3d4cd7
SpectrumAnalyzer <profitsw2000@gmail.com>
2026-05-08 14:23:25 +0300

    В интерфейс BluetoothPacketManager.kt добавлены переменные и функции, описывающие основные аспекты работы менеджера блютуз.

--- data/src/main/java/ru/profitsw2000/data/domain/bluetooth/BluetoothPacketManager.kt
+++ data/src/main/java/ru/profitsw2000/data/domain/bluetooth/BluetoothPacketManager.kt
@@ -2,4 +2,18 @@ package ru.profitsw2000.data.domain.bluetooth
 
 interface BluetoothPacketManager {
 
+    val RING_BUFFER_SIZE: Int
+    val ringBuffer: MutableList<Byte>
+    val packetBuffer: MutableList<Byte>
+    var bufferTail: Int
+    var bufferHead: Int
+
+    var packetState: Int
+    var packetNumber: Int
+    var packetSize: Int
+    var packetCheckSum: Int
+
+    fun insertBytes(byteArray: ByteArray)
+    fun parseBuffer()
+
 }
