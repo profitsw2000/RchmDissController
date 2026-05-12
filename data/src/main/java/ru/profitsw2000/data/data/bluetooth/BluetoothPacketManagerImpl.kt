521399b942d016f5d383346e98d09c72f5763aaf
profitsw2000 <profitsw2000@gmail.com>
2026-05-12 21:11:02 +0300

    Реализация интерфейса менеджера пакетов блютуз - начало.

--- data/src/main/java/ru/profitsw2000/data/data/bluetooth/BluetoothPacketManagerImpl.kt
+++ data/src/main/java/ru/profitsw2000/data/data/bluetooth/BluetoothPacketManagerImpl.kt
@@ -1,34 +1,42 @@
 package ru.profitsw2000.data.data.bluetooth
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import ru.profitsw2000.data.domain.bluetooth.BluetoothPacketManager
 import ru.profitsw2000.data.domain.bluetooth.BluetoothRepository
 
 class BluetoothPacketManagerImpl(
-    bluetoothRepository: BluetoothRepository
+    private val bluetoothRepository: BluetoothRepository
 ) : BluetoothPacketManager {
-    override val BUFFER_SIZE: Int
-        get() = TODO("Not yet implemented")
-    override val packetBuffer: MutableList<Byte>
-        get() = TODO("Not yet implemented")
-    override var packetState: Int
-        get() = TODO("Not yet implemented")
-        set(value) {}
-    override var packetNumber: Int
-        get() = TODO("Not yet implemented")
-        set(value) {}
-    override var packetSize: Int
-        get() = TODO("Not yet implemented")
-        set(value) {}
-    override var packetCheckSum: Int
-        get() = TODO("Not yet implemented")
-        set(value) {}
+
+    private val coroutineScope = CoroutineScope(Job() + Dispatchers.Default)
+
+    override val BUFFER_SIZE: Int = 16
+    override val packetBuffer: MutableList<Byte> = arrayListOf()
+    override var packetState = 0
+    override var packetNumber = 0
+    override var packetSize = 0
+    override var packetCheckSum = 0
 
     override fun observeBluetoothDataBytesFlow() {
-        TODO("Not yet implemented")
+        coroutineScope.launch {
+            bluetoothRepository.bluetoothBytesDataFlow.collect { bytes ->
+                parseIncomingFlow(bytes)
+            }
+        }
     }
 
     override fun parseIncomingFlow(byteArray: ByteArray) {
-        TODO("Not yet implemented")
+        byteArray.forEach { byte ->
+            when(packetState) {
+                0 -> checkStartByte(byte)
+                1 -> checkPacketSize(byte)
+                2 -> checkPacketId(byte)
+                else -> getPacketData(byte)
+            }
+        }
     }
 
     override fun checkStartByte(byte: Byte) {
