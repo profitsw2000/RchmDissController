16157284d4a1ad7058ab727a838cf0a3cbac3182
SpectrumAnalyzer <profitsw2000@gmail.com>
2026-05-20 16:51:06 +0300

    Во вью моделе главного фрагмента создана функция по отправке настроек на РЧМ-ДИСС.

--- mainScreen/src/main/java/ru/profitsw2000/mainscreen/presentation/viewmodel/MainViewModel.kt
+++ mainScreen/src/main/java/ru/profitsw2000/mainscreen/presentation/viewmodel/MainViewModel.kt
@@ -1,12 +1,22 @@
 package ru.profitsw2000.mainscreen.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import ru.profitsw2000.data.domain.bluetooth.BluetoothRepository
 import ru.profitsw2000.data.domain.state.RchmDissStateRepository
+import ru.profitsw2000.mainscreen.state.RchmDissUpdatingStatus
 
 class MainViewModel(
     private val rchmDissStateRepository: RchmDissStateRepository,
     private val bluetoothRepository: BluetoothRepository
 ): ViewModel() {
 
+    private val _rchmDissUpdatingStatus = MutableStateFlow<RchmDissUpdatingStatus>(
+        RchmDissUpdatingStatus.Idle)
+    val rchmDissUpdatingStatus: StateFlow<RchmDissUpdatingStatus> = _rchmDissUpdatingStatus
+
+    fun updateTransmitter(byte: Byte) {
+
+    }
 }
