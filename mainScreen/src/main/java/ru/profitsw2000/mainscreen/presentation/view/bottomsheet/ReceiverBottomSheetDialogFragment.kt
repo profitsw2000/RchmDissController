2cbd7223d69d90f895f8147f7ef9e27d84806a08
profitsw2000 <profitsw2000@gmail.com>
2026-06-04 21:17:58 +0300

    В диалоговое окно настройки субмодуля ПРМ добавлена обработка ошибки при отправке и получении ответного пакета.

--- mainScreen/src/main/java/ru/profitsw2000/mainscreen/presentation/view/bottomsheet/ReceiverBottomSheetDialogFragment.kt
+++ mainScreen/src/main/java/ru/profitsw2000/mainscreen/presentation/view/bottomsheet/ReceiverBottomSheetDialogFragment.kt
@@ -5,14 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.progressindicator.CircularProgressIndicatorSpec
+import com.google.android.material.progressindicator.IndeterminateDrawable
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.activityViewModel
+import ru.profitsw2000.core.drawable.utils.RESPONSE_PACKET_TIMEOUT_ERROR_CODE
 import ru.profitsw2000.mainscreen.databinding.FragmentReceiverBottomSheetDialogBinding
+import ru.profitsw2000.mainscreen.presentation.viewmodel.MainViewModel
+import ru.profitsw2000.mainscreen.state.ReceiverUpdatingStatus
+import kotlin.getValue
 
 class ReceiverBottomSheetDialogFragment : BottomSheetDialogFragment() {
 
     private var _binding: FragmentReceiverBottomSheetDialogBinding? = null
     private val binding get() = _binding!!
+    private val mainViewModel: MainViewModel by activityViewModel()
+    private val spec = CircularProgressIndicatorSpec(requireContext(), null).apply {
+        indicatorSize = 24.dpToPx()
+        trackThickness = 3.dpToPx()
+
+        indicatorColors = intArrayOf(resources.getColor(ru.profitsw2000.core.R.color.splashed_white))
+    }
+    val progressIndicator = IndeterminateDrawable.createCircularDrawable(requireContext(), spec)
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,7 +52,58 @@ class ReceiverBottomSheetDialogFragment : BottomSheetDialogFragment() {
         initViews()
     }
 
-    private fun initViews() {
+    private fun initViews() = with(binding) {
+        transmitterParamsSendButton.setOnClickListener {
+            //mainViewModel.updateReceiver()
+        }
+    }
 
+    private fun observeFlows() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mainViewModel.receiverUpdatingStatusFlow.collect { state ->
+                    when(state) {
+                        is ReceiverUpdatingStatus.Error -> handleError(state.errorCode)
+                        ReceiverUpdatingStatus.Idle -> TODO()
+                        is ReceiverUpdatingStatus.Success -> TODO()
+                        ReceiverUpdatingStatus.Updating -> TODO()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun handleError(errorCode: Int) = with(binding) {
+        val statusText = when(errorCode) {
+            RESPONSE_PACKET_TIMEOUT_ERROR_CODE -> ru.profitsw2000.core.R.string.response_packet_timeout_error.toString()
+            else -> ru.profitsw2000.core.R.string.unknown_error.toString()
+        }
+        setProgressBar(false)
+        setStatusText(resources.getColor(ru.profitsw2000.core.R.color.scarlet), statusText)
+    }
+
+    private fun setProgressBar(isUpdating: Boolean) = with(binding.transmitterParamsSendButton) {
+        if (isUpdating) {
+            text = ""
+            icon = progressIndicator
+            progressIndicator.start()
+            isEnabled = false
+        } else {
+            progressIndicator.stop()
+            icon = null
+            text = resources.getString(ru.profitsw2000.core.R.string.send_button_text)
+            isEnabled = true
+        }
+    }
+
+    private fun setStatusText(color: Int, statusText: String) = with(binding.updatingStatusResultTextView) {
+        visibility = View.VISIBLE
+        setTextColor(color)
+        text = statusText
+    }
+
+    private fun Int.dpToPx(): Int {
+        val density = android.content.res.Resources.getSystem().displayMetrics.density
+        return (this * density).toInt()
     }
 }
