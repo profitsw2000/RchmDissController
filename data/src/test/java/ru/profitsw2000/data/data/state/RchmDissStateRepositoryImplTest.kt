ab736fc4b1df33f4deba8e19b5eacdd82b5bcb95
SpectrumAnalyzer <profitsw2000@gmail.com>
2026-06-22 14:36:53 +0300

    Сделаны тесты состояния синтезатора для класса RchmDissStateRepositoryImpl.

--- data/src/test/java/ru/profitsw2000/data/data/state/RchmDissStateRepositoryImplTest.kt
+++ data/src/test/java/ru/profitsw2000/data/data/state/RchmDissStateRepositoryImplTest.kt
@@ -63,86 +63,90 @@ class RchmDissStateRepositoryImplTest {
         }
     }
 
+    //6
     @Test
     fun `приёмник включён канал 2, заперт канал 2, включены секции 8 и 16 дБ, включён пилот-сигнал`() = runTest {
         val testDispatcher = StandardTestDispatcher(testScheduler)
         val repository = RchmDissStateRepositoryImpl(defaultDispatcher = testDispatcher)
 
         repository.lastPacket.test {
-            repository.updateReceiverModuleState(0x58.toByte(), 0x34)
+            repository.updateReceiverModuleState(0x90.toByte(), 0xDD.toByte())
             assertThat(awaitItem()).isEqualTo(RcdInputPacketType.ReceiverStateInputPacket)
-            assertThat(repository.rchmDissState.value.receiverModuleState)
+            assertThat(repository.rchmDissState.value.receiverModuleState.toString())
                 .isEqualTo(
                     ReceiverModuleState(
                         enabledChannelNumber = 2,
                         testSignalIsEnabled = true,
                         lockedInputChannels = booleanArrayOf(false, true, false, false, false),
                         inputAttenuationValue = 24,
-                        inputAttenuatorsCode = 0x35
-                    )
+                        inputAttenuatorsCode = 0x180
+                    ).toString()
                 )
         }
     }
 
+    //7
     @Test
     fun `тест приёмника включён канал 3, заперт канал 4, 5, включены секции 2 и 32 дБ, включён пилот-сигнал`() = runTest {
         val testDispatcher = StandardTestDispatcher(testScheduler)
         val repository = RchmDissStateRepositoryImpl(defaultDispatcher = testDispatcher)
 
         repository.lastPacket.test {
-            repository.updateReceiverModuleState(0x58.toByte(), 0x34)
+            repository.updateReceiverModuleState(0x46.toByte(), 0xEE.toByte())
             assertThat(awaitItem()).isEqualTo(RcdInputPacketType.ReceiverStateInputPacket)
-            assertThat(repository.rchmDissState.value.receiverModuleState)
+            assertThat(repository.rchmDissState.value.receiverModuleState.toString())
                 .isEqualTo(
                     ReceiverModuleState(
                         enabledChannelNumber = 3,
                         testSignalIsEnabled = true,
                         lockedInputChannels = booleanArrayOf(false, false, false, true, true),
                         inputAttenuationValue = 34,
-                        inputAttenuatorsCode = 0x35
-                    )
+                        inputAttenuatorsCode = 0x240
+                    ).toString()
                 )
         }
     }
 
+    //8
     @Test
     fun `тест приёмника включён канал 4, заперт канал 1,5, включены секции 32,8,16 дБ, выключен пилот-сигнал`() = runTest {
         val testDispatcher = StandardTestDispatcher(testScheduler)
         val repository = RchmDissStateRepositoryImpl(defaultDispatcher = testDispatcher)
 
         repository.lastPacket.test {
-            repository.updateReceiverModuleState(0x58.toByte(), 0x34)
+            repository.updateReceiverModuleState(0xA2.toByte(), 0x77)
             assertThat(awaitItem()).isEqualTo(RcdInputPacketType.ReceiverStateInputPacket)
-            assertThat(repository.rchmDissState.value.receiverModuleState)
+            assertThat(repository.rchmDissState.value.receiverModuleState.toString())
                 .isEqualTo(
                     ReceiverModuleState(
                         enabledChannelNumber = 4,
                         testSignalIsEnabled = false,
                         lockedInputChannels = booleanArrayOf(true, false, false, false, true),
                         inputAttenuationValue = 56,
-                        inputAttenuatorsCode = 0x35
-                    )
+                        inputAttenuatorsCode = 0x380
+                    ).toString()
                 )
         }
     }
 
+    //9
     @Test
     fun `тест приёмника все каналы выключены, все заперты, выключены все секции аттенюатора, выключен пилот-сигнал`() = runTest {
         val testDispatcher = StandardTestDispatcher(testScheduler)
         val repository = RchmDissStateRepositoryImpl(defaultDispatcher = testDispatcher)
 
         repository.lastPacket.test {
-            repository.updateReceiverModuleState(0x58.toByte(), 0x34)
+            repository.updateReceiverModuleState(0x3E.toByte(), 0x7C.toByte())
             assertThat(awaitItem()).isEqualTo(RcdInputPacketType.ReceiverStateInputPacket)
-            assertThat(repository.rchmDissState.value.receiverModuleState)
+            assertThat(repository.rchmDissState.value.receiverModuleState.toString())
                 .isEqualTo(
                     ReceiverModuleState(
                         enabledChannelNumber = 0,
                         testSignalIsEnabled = false,
                         lockedInputChannels = booleanArrayOf(true, true, true, true, true),
                         inputAttenuationValue = 0,
-                        inputAttenuatorsCode = 0x35
-                    )
+                        inputAttenuatorsCode = 0
+                    ).toString()
                 )
         }
     }
@@ -190,6 +194,80 @@ class RchmDissStateRepositoryImplTest {
                         lfm2Register = listOf(0x3FA0F9, 0x300000),
                         lfm3Register = listOf(0x500004, 0x500000),
                         prwRegister = 0x700000,
+                        praRegister = 0x900000
+                    )
+                )
+        }
+    }
+
+    @Test
+    fun `тест синтезатора симметричная ЛЧМ`() = runTest {
+        val testDispatcher = StandardTestDispatcher(testScheduler)
+        val repository = RchmDissStateRepositoryImpl(defaultDispatcher = testDispatcher)
+
+        repository.lastPacket.test {
+            repository.updateSynthesizerModuleState(0x00, 0x00, 0x70)
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x01, 0x00, 0x00)
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0xA5.toByte(), 0x00, 0x20)
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x20, 0x4E.toByte(), 0x40)
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x00, 0x7D, 0x60)
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x08, 0x06, 0x84.toByte())
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x02, 0x00, 0xA0.toByte())
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x01, 0x00, 0xC0.toByte())
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0xF0.toByte(), 0x00, 0x10)
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x7C.toByte(), 0xA0.toByte(), 0x3F.toByte())
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x04.toByte(), 0x02, 0x50)
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+
+            repository.updateSynthesizerModuleState(0x00, 0x40, 0x70)
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x01, 0x00, 0x00)
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0xA7.toByte(), 0x00, 0x20)
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x80.toByte(), 0x3E.toByte(), 0x40)
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x00, 0x7D, 0x60)
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x08, 0x06, 0x84.toByte())
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x02, 0x00, 0xA0.toByte())
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x01, 0x00, 0xC0.toByte())
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0xF0.toByte(), 0x00, 0x10)
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x7C.toByte(), 0xA0.toByte(), 0x3F.toByte())
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x06.toByte(), 0x00, 0x50)
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+            repository.updateSynthesizerModuleState(0x00, 0x00, 0x90.toByte())
+            assertThat(awaitItem()).isEqualTo(RcdInputPacketType.SynthesizerStateInputPacket)
+
+            assertThat(repository.rchmDissState.value.synthesizerModuleState)
+                .isEqualTo(
+                    SynthesizerModuleState().copy(
+                        refRegister = listOf(0x01, 0x01),
+                        intRegister = listOf(0x2000A5, 0x2000A7),
+                        fracRegister = listOf(0x404E20, 0x403E80),
+                        modRegister = listOf(0x607D00, 0x607D00),
+                        ctr1Register = listOf(0x840608, 0x840608),
+                        ctr2Register = listOf(0xA00002, 0xA00002),
+                        ctr3Register = listOf(0xC00001, 0xC00001),
+                        lfm1Register = listOf(0x1000F0, 0x1000F0),
+                        lfm2Register = listOf(0x3FA07C, 0x3FA07C),
+                        lfm3Register = listOf(0x500204, 0x500006),
+                        prwRegister = 0x704000,
                         praRegister = 0x900000
                     )
                 )
